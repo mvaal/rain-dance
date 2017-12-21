6fda6c1054cb4af653c7efd46ad80db74284f40c
Marcus Vaal <mjvaal@gmail.com>
2017-12-20 20:49:41 -0600

    Added missing tests from AccuLocationSpec

--- src/test/scala/com/tp/rd/bot/accuweather/model/AccuLocationSpec.scala
+++ src/test/scala/com/tp/rd/bot/accuweather/model/AccuLocationSpec.scala
@@ -1,6 +1,8 @@
 package com.tp.rd.bot.accuweather.model
 
-import com.tp.rd.weather.accuweather.model.AccuLocation
+import java.io.ByteArrayInputStream
+
+import com.tp.rd.weather.accuweather.model.{AccuLocation, ObjectMapper}
 import org.scalatest.{FlatSpec, Matchers}
 
 class AccuLocationSpec extends FlatSpec with Matchers {
@@ -9,5 +11,14 @@ class AccuLocationSpec extends FlatSpec with Matchers {
     val expectedKey = "ExpectedKey"
     val accuLocation = AccuLocation(expectedKey)
     accuLocation.locationKey should be(expectedKey)
+  }
+
+  behavior of "parseAccuLocation"
+  it should "parse properly from InputStream" in {
+    val expectedKey = "ExpectedKey"
+    val accuLocation = AccuLocation(expectedKey)
+    val byteArray = ObjectMapper.writeValueAsBytes(accuLocation)
+    val inputStream = new ByteArrayInputStream(byteArray)
+    AccuLocation.parseAccuLocation(inputStream) should be(accuLocation)
   }
 }
